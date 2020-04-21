f9cc7d88626e566d0986f2909938c1d2c9f12ca2
Joe Sweeney <joe@j1.io>
2020-04-21 03:06:55 -0400

    Genericize Docker container shell

--- artifacts/setup_ssh.sh
+++ artifacts/setup_ssh.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-DEPLOY_KEY_VARIABLE="${DEPLOY_KEY_ENV_VAR_PREFIX}_key"
-DEPLOY_IV_VARIABLE="${DEPLOY_KEY_ENV_VAR_PREFIX}_iv"
-
-openssl aes-256-cbc -K "${!DEPLOY_KEY_VARIABLE}" -iv "${!DEPLOY_IV_VARIABLE}" -in "${DEPLOY_ARTIFACTS_DIR}"/deploy_key.enc -out "${DEPLOY_ARTIFACTS_DIR}"/deploy_key -d
+openssl aes-256-cbc -K "${DEPLOY_KEY}" -iv "${DEPLOY_KEY_IV}" -in "${DEPLOY_ARTIFACTS_DIR}"/deploy_key.enc -out "${DEPLOY_ARTIFACTS_DIR}"/deploy_key -d
 
 chmod 600 "${DEPLOY_ARTIFACTS_DIR}"/deploy_key
 
