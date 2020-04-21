60bb1173bcb83306218fcc9d9b84ec4b808a70c1
Joe Sweeney <joe@j1.io>
2020-04-21 18:11:32 -0400

    Update env var names

--- artifacts/setup_ssh.sh
+++ artifacts/setup_ssh.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K "${ENCRYPTED_DEPLOY_KEY_CYPHER_KEY}" -iv "${ENCRYPTED_DEPLOY_KEY_IV}" -in "${ENCRYPTED_DEPLOY_KEY_PATH}" -out "${DEPLOY_ARTIFACTS_DIR}"/deploy_key -d
+openssl aes-256-cbc -K "${ENCRYPTED_DEPLOY_KEY_CYPHER_KEY}" -iv "${ENCRYPTED_DEPLOY_KEY_IV}" -in "${ENCRYPTED_DEPLOY_KEY_PATH}" -out "${DEPLOY_ARTIFACTS_PATH}"/deploy_key -d
 
-chmod 600 "${DEPLOY_ARTIFACTS_DIR}"/deploy_key
+chmod 600 "${DEPLOY_ARTIFACTS_PATH}"/deploy_key
 
-ssh-add "${DEPLOY_ARTIFACTS_DIR}"/deploy_key
+ssh-add "${DEPLOY_ARTIFACTS_PATH}"/deploy_key
 
 { ssh-keyscan -t "$SSH_KEY_TYPES" -H "$DEPLOY_SERVER" >> "${TRAVIS_HOME}"/.ssh/known_hosts; } 2>&1
