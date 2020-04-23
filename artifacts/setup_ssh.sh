05d8cd2b0ffd17a66ee787a28a308263eefdf89d
Joe Sweeney <joe@j1.io>
2020-04-23 15:12:38 -0400

    Use utils functions

--- artifacts/setup_ssh.sh
+++ artifacts/setup_ssh.sh
@@ -4,24 +4,9 @@
 source "$(dirname "$0")"/utils.sh
 
 verify_var_set 'ENCRYPTED_DEPLOY_KEY_CYPHER_KEY'
-
-if [ -z "$ENCRYPTED_DEPLOY_KEY_IV" ]
-then
-  echo '[ERROR] ENCRYPTED_DEPLOY_KEY_IV is blank or unset! Exiting...' 1>&2
-  exit 1
-fi
-
-if [ -z "$ENCRYPTED_DEPLOY_KEY_PATH" ]
-then
-  echo '[ERROR] ENCRYPTED_DEPLOY_KEY_PATH is blank or unset! Exiting...' 1>&2
-  exit 1
-fi
-
-if [ -z "$DEPLOY_ARTIFACTS_PATH" ]
-then
-  echo '[ERROR] ENCRYPTED_DEPLOY_KEY_PATH is blank or unset! Exiting...' 1>&2
-  exit 1
-fi
+verify_var_set 'ENCRYPTED_DEPLOY_KEY_IV'
+verify_var_set 'ENCRYPTED_DEPLOY_KEY_PATH'
+verify_var_set 'DEPLOY_ARTIFACTS_PATH'
 
 openssl aes-256-cbc -K "${ENCRYPTED_DEPLOY_KEY_CYPHER_KEY}" -iv "${ENCRYPTED_DEPLOY_KEY_IV}" -in "${ENCRYPTED_DEPLOY_KEY_PATH}" -out "${DEPLOY_ARTIFACTS_PATH}"/deploy_key -d
 
@@ -29,16 +14,7 @@ chmod 600 "${DEPLOY_ARTIFACTS_PATH}"/deploy_key
 
 ssh-add "${DEPLOY_ARTIFACTS_PATH}"/deploy_key
 
-if [ -z "$SSH_KEY_TYPES" ]
-then
-  echo '[ERROR] SSH_KEY_TYPES is blank or unset! Exiting...' 1>&2
-  exit 1
-fi
-
-if [ -z "$DEPLOY_SERVER" ]
-then
-  echo '[ERROR] DEPLOY_SERVER is blank or unset! Exiting...' 1>&2
-  exit 1
-fi
+verify_var_set 'SSH_KEY_TYPES'
+verify_var_set 'DEPLOY_SERVER'
 
 { ssh-keyscan -t "$SSH_KEY_TYPES" -H "$DEPLOY_SERVER" >> "${TRAVIS_HOME}"/.ssh/known_hosts; } 2>&1
