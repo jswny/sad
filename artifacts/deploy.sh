446b2adba0d1c8846f7c35568123008816551609
Joe Sweeney <joe@j1.io>
2020-04-23 17:57:52 -0400

    Add more env var safeguards

--- artifacts/deploy.sh
+++ artifacts/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+verify_var_set 'BRANCH'
+verify_var_set 'BETA_BRANCH'
+
 if [ "$BRANCH" = "$BETA_BRANCH" ]
 then
   DEPLOY_CHANNEL='beta'
@@ -10,22 +13,25 @@ else
   echo "[ERROR] Unsupported branch $BRANCH" 1>&2
   exit 1
 fi
+verify_var_set 'DEPLOY_CHANNEL'
 
 DISCORD_TOKEN_VARIABLE=$(echo "DISCORD_TOKEN_${DEPLOY_CHANNEL}" | tr '[:lower:]' '[:upper:]')
 DISCORD_TOKEN="${!DISCORD_TOKEN_VARIABLE}"
 
-if [ -z "$DISCORD_TOKEN" ]
-then
-  echo "[ERROR] $DISCORD_TOKEN_VARIABLE is blank or unset! Exiting..." 1>&2
-  exit 1
-fi
+verify_var_set 'DEPLOY_ROOT_DIR'
+verify_var_set 'REPOSITORY'
 
-DEPLOY_DIR="${DEPLOY_ROOT_DIR}"/"${REPOSITORY}"-"$DEPLOY_CHANNEL"
+DEPLOY_DIR="${DEPLOY_ROOT_DIR}"/"${REPOSITORY}"-"${DEPLOY_CHANNEL}"
 
+verify_var_set 'DEPLOY_USERNAME'
+verify_var_set 'DEPLOY_SERVER'
+verify_var_set 'DEPLOY_DIR'
 ssh "${DEPLOY_USERNAME}"@"$DEPLOY_SERVER" mkdir "$DEPLOY_DIR"
 
 scp docker-compose.yml "${DEPLOY_USERNAME}"@"$DEPLOY_SERVER":"$DEPLOY_DIR"
 
+verify_var_set 'DISCORD_TOKEN'
+verify_var_set 'DOCKER_IMAGE_TAG'
 {
   echo "DISCORD_TOKEN=${DISCORD_TOKEN}"
   echo "TAG=${DOCKER_IMAGE_TAG}"
