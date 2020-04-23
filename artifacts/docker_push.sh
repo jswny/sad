9afed6a944f87006bcbe306c91e29070044fbe79
Joe Sweeney <joe@j1.io>
2020-04-23 17:42:29 -0400

    Add more env var safeguards

--- artifacts/docker_push.sh
+++ artifacts/docker_push.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+# shellcheck source=artifacts/utils.sh
+source "$(dirname "$0")"/utils.sh
+
+verify_var_set 'DOCKER_PASSWORD'
+verify_var_set 'DOCKER_USERNAME'
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+verify_var_set 'REPOSITORY'
+
 docker build --tag "$REPOSITORY" .
+
+verify_var_set 'DOCKER_IMAGE_NAME'
+
 docker tag "$REPOSITORY" "$DOCKER_IMAGE_NAME"
+
 docker push "$DOCKER_IMAGE_NAME"
