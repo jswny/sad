8496b901da22a6dd1b75e74094d82847aa0ab906
Joe Sweeney <joe@j1.io>
2020-04-27 01:51:08 -0400

    Add support for custom env vars

--- artifacts/deploy.sh
+++ artifacts/deploy.sh
@@ -16,13 +16,12 @@ else
   echo "[ERROR] Unsupported branch $BRANCH" 1>&2
   exit 1
 fi
-verify_var_set 'DEPLOY_CHANNEL'
 
-DISCORD_TOKEN_VARIABLE=$(echo "DISCORD_TOKEN_${DEPLOY_CHANNEL}" | tr '[:lower:]' '[:upper:]')
-DISCORD_TOKEN="${!DISCORD_TOKEN_VARIABLE}"
+echo "Deploying to channel $DEPLOY_CHANNEL..."
 
 verify_var_set 'DEPLOY_ROOT_DIR'
 verify_var_set 'REPOSITORY'
+verify_var_set 'DEPLOY_CHANNEL'
 
 DEPLOY_DIR="${DEPLOY_ROOT_DIR}"/"${REPOSITORY}"-"${DEPLOY_CHANNEL}"
 
@@ -33,14 +32,24 @@ ssh "${DEPLOY_USERNAME}"@"$DEPLOY_SERVER" mkdir "$DEPLOY_DIR"
 
 scp docker-compose.yml "${DEPLOY_USERNAME}"@"$DEPLOY_SERVER":"$DEPLOY_DIR"
 
-verify_var_set 'DISCORD_TOKEN'
+IFS=', ' read -r -a DEPLOY_CHANNEL_VAR_PREFIXES_ARRAY <<< "$DEPLOY_CHANNEL_VAR_PREFIXES"
+
 verify_var_set 'DOCKER_IMAGE_TAG'
 {
-  echo "DISCORD_TOKEN=${DISCORD_TOKEN}"
   echo "TAG=${DOCKER_IMAGE_TAG}"
   echo "DOCKER_USERNAME=${DOCKER_USERNAME}"
   echo "REPOSITORY=${REPOSITORY}"
 } >> ".env"
+
+for VAR_PREFIX in "${DEPLOY_CHANNEL_VAR_PREFIXES_ARRAY[@]}"
+do
+  VAR_NAME=$(echo "DISCORD_TOKEN_${DEPLOY_CHANNEL}" | tr '[:lower:]' '[:upper:]')
+  VAR_VALUE="${!VAR_NAME}"
+  verify_var_set 'VAR_VALUE'
+  echo "Setting deploy variable $VAR_NAME..."
+  echo "${VAR_PREFIX}=${VAR_VALUE}" >> ".env"
+done
+
 scp ".env" "${DEPLOY_USERNAME}"@"$DEPLOY_SERVER":"$DEPLOY_DIR"
 
 ssh "${DEPLOY_USERNAME}"@"$DEPLOY_SERVER" "docker pull '${DOCKER_IMAGE_NAME}'"
