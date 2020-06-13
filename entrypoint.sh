9f8d745a20543cf06ff2142ecc86ff845ac72002
Joe Sweeney <joe@j1.io>
2020-06-13 02:57:09 -0400

    Detect local image name and tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,11 +43,20 @@ cat "/github/workspace/$app_path/.gitignore"
 
 docker images
 
-LOCAL_IMAGE_ID="$(docker images -q "$GITHUB_REPOSITORY" 2> /dev/null)"
+local_image_id="$(docker images -q "$GITHUB_REPOSITORY" 2> /dev/null)"
 
-if [[ "$LOCAL_IMAGE_ID" == "" ]]; then
+if [[ "$local_image_id" == "" ]]; then
   log 'error' 'No local Docker image detected for this repository! Please build a local image first before deploying!'
   exit 1
 fi
 
-log 'debug' "Local Docker image ID: $LOCAL_IMAGE_ID"
+log 'debug' "Local Docker image ID: $local_image_id"
+
+local_image_name_and_tag="$(docker inspect --format='{{ (index .RepoTags 0) }}' $local_image_id 2> /dev/null)"
+
+if [[ "$local_image_name_and_tag" == "" ]]; then
+  log 'error' 'Could not find the local Docker image name and tag!'
+  exit 1
+fi
+
+log 'debug' "Local Docker image name and tag: $local_image_name_and_tag"
