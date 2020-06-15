f58206e84dc3d43b9f957e4805e7384fef1cf583
Joe Sweeney <joe@j1.io>
2020-06-15 00:27:36 -0400

    Add brackets to env vars in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,41 +6,41 @@ set -eo pipefail
 log() {
   local prefix_spacer="-----"
   local component="Deploy"
-  local prefix="$prefix_spacer [$component]"
-  if [ "$1" = 'debug' ]; then
+  local prefix="${prefix_spacer} [${component}]"
+  if [ "${1}" = 'debug' ]; then
     if [ "$debug" = 1 ]; then
-      echo "$prefix DEBUG: $2"
+      echo "$prefix DEBUG: ${2}"
     fi
-  elif [ "$1" = 'info' ]; then
-    echo "$prefix INFO: $2"
-  elif [ "$1" = 'warn' ]; then
-    echo "$prefix WARN: $2" >&2
-  elif [ "$1" = 'error' ]; then
-    echo "$prefix ERROR: $2" >&2
+  elif [ "${1}" = 'info' ]; then
+    echo "$prefix INFO: ${2}"
+  elif [ "${1}" = 'warn' ]; then
+    echo "$prefix WARN: ${2}" >&2
+  elif [ "${1}" = 'error' ]; then
+    echo "$prefix ERROR: ${2}" >&2
   else
-    echo "$prefix INTERNAL ERROR: invalid option \"$1\" for log() with message \"$2\"" >&2 "" >&2
+    echo "$prefix INTERNAL ERROR: invalid option \"${1}\" for log() with message \"${2}\"" >&2 "" >&2
   fi
 }
 
 verify_var_set() {
   if [ -z "${!1}" ]; then
-    if [ -z "$2" ]; then
-      log 'error' "\"$1\" is blank or unset!"
+    if [ -z "${2}" ]; then
+      log 'error' "\"${1}\" is blank or unset!"
     else
-      log 'error' "$2"
+      log 'error' "${2}"
     fi
     exit 1
   fi
 }
 
 # Translate input environment variables
-deploy_server="$INPUT_DEPLOY_SERVER"
-deploy_username="$INPUT_DEPLOY_USERNAME"
-deploy_password="$INPUT_DEPLOY_PASSWORD"
-deploy_root_dir="$INPUT_DEPLOY_ROOT_DIR"
-encrypted_deploy_key_encryption_key="$INPUT_ENCRYPTED_DEPLOY_KEY_ENCRYPTION_KEY"
-app_path="$INPUT_PATH"
-debug="$INPUT_DEBUG"
+deploy_server="${INPUT_DEPLOY_SERVER}"
+deploy_username="${INPUT_DEPLOY_USERNAME}"
+deploy_password="${INPUT_DEPLOY_PASSWORD}"
+deploy_root_dir="${INPUT_DEPLOY_ROOT_DIR}"
+encrypted_deploy_key_encryption_key="${INPUT_ENCRYPTED_DEPLOY_KEY_ENCRYPTION_KEY}"
+app_path="${INPUT_PATH}"
+debug="${INPUT_DEBUG}"
 
 ssh_key_types='rsa,dsa,ecdsa'
 
@@ -56,17 +56,17 @@ verify_var_set 'ssh_key_types'
 
 cat "/github/workspace/${app_path}/.gitignore"
 
-local_image_id="$(docker images -q "$GITHUB_REPOSITORY" 2> /dev/null)"
+local_image_id="$(docker images -q "${GITHUB_REPOSITORY}" 2> /dev/null)"
 
 verify_var_set 'local_image_id' 'No local Docker image detected for this repository! Please build a local image first before deploying!'
 
-log 'debug' "Local Docker image ID: \"$local_image_id\""
+log 'debug' "Local Docker image ID: \"${local_image_id}\""
 
-local_image="$(docker inspect --format='{{ (index .RepoTags 0) }}' "$local_image_id" 2> /dev/null)"
+local_image="$(docker inspect --format='{{ (index .RepoTags 0) }}' "${local_image_id}" 2> /dev/null)"
 
 verify_var_set 'local_image' 'Could not find the local Docker image name and tag!'
 
-log 'debug' "Local Docker image name and tag: $local_image"
+log 'debug' "Local Docker image name and tag: ${local_image}"
 
 openssl enc -aes-256-cbc -d -in "${app_path}/deploy_key.enc" -out deploy_key -k "${encrypted_deploy_key_encryption_key}"
 
@@ -74,4 +74,4 @@ chmod 600 'deploy_key'
 
 ssh-add 'deploy_key'
 
-{ ssh-keyscan -t "$ssh_key_types" -H "$deploy_server" >> "${HOME}/.ssh/known_hosts"; } 2>&1
+{ ssh-keyscan -t "${ssh_key_types}" -H "$deploy_server" >> "${HOME}/.ssh/known_hosts"; } 2>&1
