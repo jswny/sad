6ecf629e7df11f989a819428a03bb7f5ac210105
Joe Sweeney <joe@j1.io>
2020-06-16 14:06:15 -0400

    Temporarily print out SSH fingerprinting

--- entrypoint.sh
+++ entrypoint.sh
@@ -102,6 +102,7 @@ ssh_path="${HOME}/.ssh"
 verify_var_set 'ssh_path'
 mkdir -p "${ssh_path}"
 
-ssh-keyscan -t "${ssh_key_types}" -H "${deploy_server}"
+ssh-keyscan -t "${ssh_key_types}" "${deploy_server}"
+ssh-keyscan -H
 
 { ssh-keyscan -t "${ssh_key_types}" -H "${deploy_server}" >> "${ssh_path}/known_hosts"; } 2>&1
