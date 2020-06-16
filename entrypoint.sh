e013f9c742a90de34ef3e7b0da963100daa61a81
Joe Sweeney <joe@j1.io>
2020-06-16 13:11:56 -0400

    Improve environment variable checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,6 +63,7 @@ verify_var_set 'deploy_root_dir'
 verify_var_set 'encrypted_deploy_key_encryption_key'
 verify_var_set 'app_path' 'path is blank or unset!'
 verify_var_set 'debug'
+verify_var_set 'repository' 'GITHUB_REPOSITORY is blank or unset!'
 verify_var_set 'ssh_key_types'
 verify_var_set 'repo_path'
 
