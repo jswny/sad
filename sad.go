06587930c5f371adb603d85c757acc20f3984d83
Joe Sweeney <joe@j1.io>
2020-08-22 16:18:48 -0400

    Add options struct

--- sad.go
+++ sad.go
@@ -1,5 +1,18 @@
 package sad
 
+import "net"
+
+type options struct {
+	Server     net.IP
+	Username   string
+	RootDir    string
+	PrivateKey interface{}
+	Channel    string
+	Path       string
+	EnvVars    []string
+	Debug      bool
+}
+
 // Hello says hello to the world
 func Hello() string {
 	return "Hello, world."
