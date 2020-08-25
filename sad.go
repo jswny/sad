397cb435d03fe33071b8e668441950ae41a609b5
Joe Sweeney <joe@j1.io>
2020-08-25 15:50:51 -0400

    Custom RSA key type, add marshal/unmarshal tests

--- sad.go
+++ sad.go
@@ -2,22 +2,57 @@ package sad
 
 import (
 	"crypto/rsa"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
+	"errors"
 	"io/ioutil"
 	"net"
 	"os"
 )
+
+type RSAPrivateKey struct {
+	PrivateKey *rsa.PrivateKey
+}
 
 // Options for deployment
 type Options struct {
 	Server     net.IP
 	Username   string
 	RootDir    string
-	PrivateKey *rsa.PrivateKey
+	PrivateKey RSAPrivateKey
 	Channel    string
 	Path       string
 	EnvVars    []string
 	Debug      bool
+}
+
+func (k RSAPrivateKey) MarshalJSON() ([]byte, error) {
+	data := x509.MarshalPKCS1PrivateKey(k.PrivateKey)
+	pemBlock := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: data,
+		},
+	)
+
+	return pemBlock, nil
+}
+
+func (k *RSAPrivateKey) UnmarshalJSON(data []byte) error {
+	block, _ := pem.Decode(data)
+
+	if block == nil {
+		return errors.New("Failed to parse PEM block containing RSA private key")
+	}
+
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return err
+	}
+
+	k.PrivateKey = privateKey
+	return err
 }
 
 // Hello says hello to the world
