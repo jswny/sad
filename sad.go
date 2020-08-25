7936ffd5f924c9b5e3ac167cfce62b2ab8bb13c2
Joe Sweeney <joe@j1.io>
2020-08-25 12:09:28 -0400

    Add tests for options parsing

--- sad.go
+++ sad.go
@@ -1,8 +1,14 @@
 package sad
 
-import "net"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net"
+	"os"
+)
 
-type options struct {
+// Options for deployment
+type Options struct {
 	Server     net.IP
 	Username   string
 	RootDir    string
@@ -16,4 +22,23 @@ type options struct {
 // Hello says hello to the world
 func Hello() string {
 	return "Hello, world."
+}
+
+// Get parses options from a file
+func (o *Options) Get(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, o)
 }
--- sad_test.go
+++ sad_test.go
@@ -1,6 +1,10 @@
 package sad_test
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net"
+	"os"
 	"testing"
 
 	"github.com/jswny/sad"
@@ -12,4 +16,91 @@ func TestHello(t *testing.T) {
 	if s != expected {
 		t.Errorf("Expected %q, got %q instead", expected, s)
 	}
+}
+
+func TestOptionsGet(t *testing.T) {
+	testOpts := sad.Options{
+		Server:     net.ParseIP("1.2.3.4"),
+		Username:   "user1",
+		RootDir:    "/srv",
+		PrivateKey: "testkey123",
+		Channel:    "beta",
+		Path:       "/app",
+		EnvVars:    []string{"foo", "bar"},
+		Debug:      true,
+	}
+
+	testOptsData, err := json.Marshal(testOpts)
+
+	if err != nil {
+		t.Fatalf("Error marshaling test options struct: %s", err)
+	}
+
+	tempFile, err := ioutil.TempFile("", ".sad.json.test.")
+
+	if err != nil {
+		t.Fatalf("Error creating temp file: %s", err)
+	}
+
+	defer os.Remove(tempFile.Name())
+
+	if err := ioutil.WriteFile(tempFile.Name(), testOptsData, 0644); err != nil {
+		t.Fatalf("Error writing to temp file: %s", err)
+	}
+
+	opts := sad.Options{}
+
+	if err := opts.Get(tempFile.Name()); err != nil {
+		t.Fatalf("Error getting options from file: %s", err)
+	}
+
+	if !opts.Server.Equal(testOpts.Server) {
+		t.Errorf("Expected server IP %s but got %s", testOpts.Server, opts.Server)
+	}
+
+	if opts.Username != testOpts.Username {
+		t.Errorf("Expected username %s but got %s", testOpts.Username, opts.Username)
+	}
+
+	if opts.RootDir != testOpts.RootDir {
+		t.Errorf("Expected root directory %s but got %s", testOpts.RootDir, opts.RootDir)
+	}
+
+	if opts.PrivateKey != testOpts.PrivateKey {
+		t.Errorf("Expected private key %s but got %s", testOpts.PrivateKey, opts.PrivateKey)
+	}
+
+	if opts.Channel != testOpts.Channel {
+		t.Errorf("Expected channel %s but got %s", testOpts.Channel, opts.Channel)
+	}
+
+	if opts.Path != testOpts.Path {
+		t.Errorf("Expected path %s but got %s", testOpts.Path, opts.Path)
+	}
+
+	if !testEqualSlices(opts.EnvVars, testOpts.EnvVars) {
+		t.Errorf("Expected environment variables %s but got %s", testOpts.EnvVars, opts.EnvVars)
+	}
+
+	if opts.Debug != testOpts.Debug {
+		t.Errorf("Expected debug %t but got %t", testOpts.Debug, opts.Debug)
+	}
+}
+
+func testEqualSlices(a, b []string) bool {
+	if (a == nil) != (b == nil) {
+		return false
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
 }
