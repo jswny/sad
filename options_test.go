14f6ee8d3937354ab1445924ad2d3c713c6ab4ce
Joe Sweeney <joe@j1.io>
2020-08-27 14:15:09 -0400

    Add tests for string to private key

--- options_test.go
+++ options_test.go
@@ -43,7 +43,7 @@ func TestRSAPrivateKeyUnmarshalJSON(t *testing.T) {
 	}
 
 	if !rsaPrivateKey.PrivateKey.Equal(rsaPrivateKey2.PrivateKey) {
-		t.Errorf("Expected marshaled and unmarshaled private keys to be equal, but they were not equal")
+		t.Errorf("Expected marshaled and unmarshaled private keys to be equal, but they were not")
 	}
 }
 
@@ -55,6 +55,22 @@ func TestToBase64PEMString(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("PEM block string was not valid base64 encoding")
+	}
+}
+
+func TestParseBase64PEMString(t *testing.T) {
+	testRSAPrivateKey := generateRSAPrivateKey()
+	encoded := testRSAPrivateKey.ToBase64PEMString()
+
+	rsaPrivateKey := sad.RSAPrivateKey{}
+	err := rsaPrivateKey.ParseBase64PEMString(encoded)
+
+	if err != nil {
+		t.Fatalf("Failed to parse base64 PEM string into an RSA private key")
+	}
+
+	if !testRSAPrivateKey.PrivateKey.Equal(rsaPrivateKey.PrivateKey) {
+		t.Errorf("Expected base64 PEM block encoded and decoded private keys to be equal, but they were not")
 	}
 }
 
