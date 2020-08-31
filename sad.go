4cc1dc7f95622ea8ca567ff7c43a202092dccfe5
Joe Sweeney <joe@j1.io>
2020-08-30 21:36:44 -0400

    Remove default sad tests

--- sad.go
+++ sad.go
@@ -1,6 +1,1 @@
 package sad
-
-// Hello says hello to the world
-func Hello() string {
-	return "Hello, world."
-}
--- sad_test.go
+++ sad_test.go
@@ -1,15 +1,1 @@
 package sad_test
-
-import (
-	"testing"
-
-	"github.com/jswny/sad"
-)
-
-func TestHello(t *testing.T) {
-	s := sad.Hello()
-	expected := "Hello, world."
-	if s != expected {
-		t.Errorf("Expected %q, got %q instead", expected, s)
-	}
-}
