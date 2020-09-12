69e6693e2902d58b36c6c408c1b3f97571345e94
Joe Sweeney <joe@j1.io>
2020-09-12 14:27:51 -0400

    Move RSA private key, tests to own file

--- options.go
+++ options.go
@@ -1,12 +1,7 @@
 package sad
 
 import (
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/base64"
 	"encoding/json"
-	"encoding/pem"
-	"errors"
 	"io/ioutil"
 	"net"
 	"os"
@@ -16,11 +11,6 @@ import (
 
 // EnvVarPrefix represents the prefix that all environment variables should have to be read properly.
 var EnvVarPrefix = "SAD_"
-
-// RSAPrivateKey wraps an RSA private key and supports conversion to/from JSON.
-type RSAPrivateKey struct {
-	PrivateKey *rsa.PrivateKey
-}
 
 // Options for deployment.
 type Options struct {
@@ -32,39 +22,6 @@ type Options struct {
 	Path       string
 	EnvVars    []string
 	Debug      bool
-}
-
-// MarshalJSON marshals an RSA private key into valid JSON.
-// The key is marshalled into a base64 encoded PEM block string.
-func (k RSAPrivateKey) MarshalJSON() ([]byte, error) {
-	encoded := k.ToBase64PEMString()
-
-	marshaledData, err := json.Marshal(encoded)
-
-	if err != nil {
-		return nil, errors.New("Failed to marshal encoded pem data to JSON")
-	}
-
-	return marshaledData, nil
-}
-
-// UnmarshalJSON unmarshals JSON into an RSA private key.
-// The key should be a base64 encoded PEM block string.
-func (k *RSAPrivateKey) UnmarshalJSON(data []byte) error {
-	var unmarshaled string
-	err := json.Unmarshal(data, &unmarshaled)
-
-	if err != nil {
-		return err
-	}
-
-	err = k.ParseBase64PEMString(unmarshaled)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 // Merge merges the other options into the existing options
@@ -194,51 +151,5 @@ func (o *Options) GetEnv() error {
 		return err
 	}
 
-	return nil
-}
-
-// ToBase64PEMString converts an RSA private key into a base 64 encoded PEM block string.
-func (k *RSAPrivateKey) ToBase64PEMString() string {
-	var data []byte
-	if k.PrivateKey != nil {
-		data = x509.MarshalPKCS1PrivateKey(k.PrivateKey)
-	}
-
-	pemBlock := pem.EncodeToMemory(
-		&pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: data,
-		},
-	)
-
-	encoded := base64.StdEncoding.EncodeToString(pemBlock)
-
-	return encoded
-}
-
-// ParseBase64PEMString parses a base64 encoded PEM block string into an RSA private key.
-func (k *RSAPrivateKey) ParseBase64PEMString(str string) error {
-	decoded, err := base64.StdEncoding.DecodeString(str)
-
-	if err != nil {
-		return err
-	}
-
-	block, _ := pem.Decode(decoded)
-
-	if block == nil {
-		return errors.New("Failed to parse PEM block containing RSA private key")
-	}
-
-	var privateKey *rsa.PrivateKey
-	if len(block.Bytes) > 0 {
-		privateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
-	}
-
-	if err != nil {
-		return err
-	}
-
-	k.PrivateKey = privateKey
 	return nil
 }
--- options_test.go
+++ options_test.go
@@ -1,7 +1,6 @@
 package sad_test
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"io/ioutil"
 	"os"
@@ -11,74 +10,6 @@ import (
 
 	"github.com/jswny/sad"
 )
-
-func TestRSAPrivateKeyMarshalJSON(t *testing.T) {
-	rsaPrivateKey := testutils.GenerateRSAPrivateKey()
-
-	data, err := rsaPrivateKey.MarshalJSON()
-
-	if err != nil {
-		t.Fatalf("Error marshaling first RSA private key: %s", err)
-	}
-
-	if !json.Valid(data) {
-		t.Errorf("RSA private key marshal to JSON did not produce valid JSON. Got %s", data)
-	}
-}
-
-func TestRSAPrivateKeyMarshalJSONNil(t *testing.T) {
-	rsaPrivateKey := sad.RSAPrivateKey{}
-
-	data, err := rsaPrivateKey.MarshalJSON()
-
-	if err != nil {
-		t.Fatalf("Error marshaling first RSA private key: %s", err)
-	}
-
-	if !json.Valid(data) {
-		t.Errorf("RSA private key marshal to JSON did not produce valid JSON. Got %s", data)
-	}
-}
-
-func TestRSAPrivateKeyUnmarshalJSON(t *testing.T) {
-	rsaPrivateKey := testutils.GenerateRSAPrivateKey()
-
-	firstKeyData, _ := rsaPrivateKey.MarshalJSON()
-
-	rsaPrivateKey2 := sad.RSAPrivateKey{}
-
-	err := rsaPrivateKey2.UnmarshalJSON(firstKeyData)
-
-	if err != nil {
-		t.Fatalf("Error unmarshaling RSA private key: %s", err)
-	}
-
-	if !rsaPrivateKey.PrivateKey.Equal(rsaPrivateKey2.PrivateKey) {
-		t.Errorf("Expected marshaled and unmarshaled private keys to be equal, but they were not")
-	}
-
-	if err := rsaPrivateKey.PrivateKey.Validate(); err != nil {
-		t.Errorf("Unmarshalled private key was not valid")
-	}
-}
-
-func TestRSAPrivateKeyUnmarshalJSONNil(t *testing.T) {
-	rsaPrivateKey := sad.RSAPrivateKey{}
-
-	firstKeyData, _ := rsaPrivateKey.MarshalJSON()
-
-	rsaPrivateKey2 := sad.RSAPrivateKey{}
-
-	err := rsaPrivateKey2.UnmarshalJSON(firstKeyData)
-
-	if err != nil {
-		t.Fatalf("Error unmarshaling RSA private key: %s", err)
-	}
-
-	if rsaPrivateKey.PrivateKey != rsaPrivateKey2.PrivateKey {
-		t.Errorf("Expected marshaled and unmarshaled private keys to be equal, but they were not")
-	}
-}
 
 func TestMergeNoEmptyValues(t *testing.T) {
 	expectedOpts := testutils.GetTestOpts()
@@ -149,33 +80,6 @@ func TestMergeDefaults(t *testing.T) {
 	expectedOpts.Path = "."
 
 	testutils.CompareOpts(expectedOpts, opts, t)
-}
-
-func TestToBase64PEMString(t *testing.T) {
-	rsaPrivateKey := testutils.GenerateRSAPrivateKey()
-	encoded := rsaPrivateKey.ToBase64PEMString()
-
-	_, err := base64.StdEncoding.DecodeString(encoded)
-
-	if err != nil {
-		t.Errorf("PEM block string was not valid base64 encoding")
-	}
-}
-
-func TestParseBase64PEMString(t *testing.T) {
-	testRSAPrivateKey := testutils.GenerateRSAPrivateKey()
-	encoded := testRSAPrivateKey.ToBase64PEMString()
-
-	rsaPrivateKey := sad.RSAPrivateKey{}
-	err := rsaPrivateKey.ParseBase64PEMString(encoded)
-
-	if err != nil {
-		t.Fatalf("Failed to parse base64 PEM string into an RSA private key")
-	}
-
-	if !testRSAPrivateKey.PrivateKey.Equal(rsaPrivateKey.PrivateKey) {
-		t.Errorf("Expected base64 PEM block encoded and decoded private keys to be equal, but they were not")
-	}
 }
 
 func TestOptionsFromStrings(t *testing.T) {
