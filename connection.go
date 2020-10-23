b7eb7e46776de1fcb71fa9349f2486fd03a1e614
Joe Sweeney <joe@j1.io>
2020-10-23 14:21:18 -0400

    Properly copy multiple files

--- connection.go
+++ connection.go
@@ -13,19 +13,13 @@ import (
 // The files are specified as a map of the name of the file to send to the server to a reader which can read the file.
 // The full path name for the file on the remote server will be generatd as <root directory as specified by options>/<app name with channel>/<file name>.
 func SendFiles(sshClient *ssh.Client, opts *Options, files map[string]io.Reader) error {
-	client, err := scp.NewClientBySSH(sshClient)
-
-	if err != nil {
-		return fmt.Errorf("error creating new SSH session for SCP using existing SSH connection: %s", err)
-	}
-
 	for fileName, reader := range files {
 		remotePath := fmt.Sprintf("%s/%s/%s", opts.RootDir, opts.GetFullAppName(), fileName)
-		permissions := "0655"
-		err = client.CopyFile(reader, remotePath, permissions)
+
+		err := copyFile(fileName, reader, remotePath, sshClient)
 
 		if err != nil {
-			return fmt.Errorf("error copying file %s to remote server: %s", fileName, err)
+			return err
 		}
 	}
 
@@ -70,4 +64,23 @@ func SSHRunCommand(client *ssh.Client, cmd string) (string, error) {
 	}
 
 	return b.String(), nil
+}
+
+func copyFile(fileName string, reader io.Reader, remotePath string, sshClient *ssh.Client) error {
+	client, err := scp.NewClientBySSH(sshClient)
+
+	if err != nil {
+		return fmt.Errorf("error creating new SSH session for SCP using existing SSH connection: %s", err)
+	}
+
+	defer client.Close()
+
+	permissions := "0655"
+	err = client.CopyFile(reader, remotePath, permissions)
+
+	if err != nil {
+		return fmt.Errorf("error copying file %s to remote server: %s", fileName, err)
+	}
+
+	return nil
 }
