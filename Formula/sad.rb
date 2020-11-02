82a8160cdc985b3224b7e1fc2aa84bf9530c3ce1
Joe Sweeney <joe@j1.io>
2020-11-02 14:41:54 -0500

    Cleanup comments in formula

--- Formula/sad.rb
+++ Formula/sad.rb
@@ -14,16 +14,6 @@ class Sad < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test sad`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    # assert_equal "system "#{bin}/sad", "-help", "something"
     assert_match "Usage of", shell_output("#{bin}/sad -help", 2).strip
   end
 end
